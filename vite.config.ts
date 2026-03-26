c1e480904575bccdd0fa5483dcd9b445e59530a8
Quang Antopho <phamngocquang3001@gmail.com>
2026-03-26 10:55:41 +0700

    Change base path from '/ANTOPHO-test/' to '/compare-doc/'

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/ANTOPHO-test/',
+    base: '/compare-doc/',
      sever: {
       port: 3000,
       host: '0.0.0.0',
